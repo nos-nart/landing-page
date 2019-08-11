f9352964fd0e61b4a70ecf5bf43bef15f7020a46
nos-nart <nosnart1510@gmail.com>
2019-08-12 00:43:03 +0700

    init

---
+++ netlify/src/main.js
@@ -0,0 +1,2 @@
+import "./css/_global.scss"
+import "./css/style.scss"
---
+++ netlify/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    theme: {
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+        },
+        colors: {
+            'dark-lighter': '#0e1e24',
+            'gray-lighter': '#717e82',
+            'gray-darkest': '#3d4852',
+            'gray-darker': '#0e1e24',
+            'white': '#fff'
+        }
+    },
+    variants: {
+        backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+        textColor: ['hover', 'focus'],
+    },
+    plugins: [
+        // Some useful comment
+    ]
+}
