60070a52a99756469a79961ae4cdca76c9e7d8ae
nos-nart <nosnart1510@gmail.com>
2019-08-16 00:24:20 +0700

    ll

--- netlify/tailwind.config.js
+++ netlify/tailwind.config.js
@@ -9,10 +9,15 @@ module.exports = {
         colors: {
             'teal': '#00ad9e',
             'dark-lighter': '#0e1e24',
-            'gray-lighter': '#717e82',
+            'gray-400': '#717e82',
             'gray-darkest': '#3d4852',
             'gray-darker': '#0e1e24',
             'white': '#fff',
+            'gray-200': '#f2f5f7',
+            'gray-300': '#e2e8f0',
+            'gray-900': '#1a202c',
+            'gray-600': '#718096',
+            'gray-700': '#4a5568'
         }
     },
     variants: {
