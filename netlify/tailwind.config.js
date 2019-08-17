ba1a7b4223c76c2c908267de36efa795d63f9cab
nos-nart <nosnart1510@gmail.com>
2019-08-18 00:30:54 +0700

    uu

--- netlify/tailwind.config.js
+++ netlify/tailwind.config.js
@@ -9,15 +9,21 @@ module.exports = {
         colors: {
             'teal': '#00ad9e',
             'dark-lighter': '#0e1e24',
-            'gray-400': '#717e82',
             'gray-darkest': '#3d4852',
             'gray-darker': '#0e1e24',
             'white': '#fff',
             'gray-200': '#f2f5f7',
             'gray-300': '#e2e8f0',
-            'gray-900': '#1a202c',
+            'gray-400': '#717e82',
+            'gray-500': '#a0aec0',
             'gray-600': '#718096',
-            'gray-700': '#4a5568'
+            'gray-700': '#4a5568',
+            'gray-900': '#1a202c',
+            'red-600': '#e53e3e'
+        },
+        inset: {
+            '0': '0rem',
+            '1': '3rem'
         }
     },
     variants: {
